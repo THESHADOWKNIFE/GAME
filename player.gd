50c4a5d9f3d7fef2a9a23c0e6bc59fba796eb96b
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-09-10 15:08:08 +1200

    9/10

--- player.gd
+++ player.gd
@@ -4,7 +4,8 @@ const RUN_SPEED := 200.0
 const JUMP_VELOCITY := -300.0
 
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
-
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta: float):
 	var direction := Input.get_axis("move_left", "move_right")
@@ -14,4 +15,15 @@ func _physics_process(delta: float):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
 	
+	if is_on_floor():
+		if is_zero_approx(direction):
+			animation_player.play("idle")
+		else:
+			animation_player.play("running")
+	else:
+		animation_player.play("jump")
+		
+	if not is_zero_approx(direction):
+		sprite_2d.flip_h = direction < 0
+		
 	move_and_slide()
---
+++ world.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var tile_map: TileMap = $TileMap
+@onready var camera_2d: Camera2D = $Player/Camera2D
+
+
+func _ready() -> void:
+	var uesd := tile_map.get_used_rect()
+	var tile_size := tile_map.tile_set.tile_size
+	
+	camera_2d.limit_top = uesd.position.y * tile_size.y
+	camera_2d.limit_right = uesd.end.x * tile_size.x
+	camera_2d.limit_bottom = uesd.end.y * tile_size.y
+	camera_2d.limit_left = uesd.position.x * tile_size.x
+	camera_2d.reset_smoothing()
